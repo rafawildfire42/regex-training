d41ab03842c8d04624059ff64fb3ebf88a0f16e6
rafael <rafaelwildfire@gmail.com>
2024-01-26 20:58:13 -0300

    feat: extraindo ip com regex

---
+++ 4_extract_ip_address.py
@@ -0,0 +1,35 @@
+# Extrair os IPs do log
+
+import re
+
+data_log = """
+Data: 2024-01-26
+Horário: 15:32:45
+IP de Origem: 192.168.1.100
+Página Acessada: /pagina-exemplo
+
+Data: 2024-01-26
+Horário: 16:45:22
+IP de Origem: 203.120.34.56
+Página Acessada: /outra-pagina
+
+Data: 2024-01-26
+Horário: 17:58:10
+IP de Origem: 72.31.45.98
+Página Acessada: /pagina-final
+"""
+
+ip_pattern = r"(\d{1,3}\.)(\d{1,3}\.)(\d{1,3}\.)(\d{1,3})"
+
+
+def extract_ips(text: str):
+    ip_occurences = re.findall(ip_pattern, text)
+    ips_extracted = ["".join(ip) for ip in ip_occurences]
+    return ips_extracted
+
+
+ips_extracted = extract_ips(data_log)
+
+
+for index, ip in enumerate(ips_extracted, 1):
+    print(f"IP {index}: {ip}")
